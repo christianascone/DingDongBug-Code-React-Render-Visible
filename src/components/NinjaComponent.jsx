b552e4dee783d7e9d4d87551db3d66cff82e7d66
Christian Ascone <ascone.christian@gmail.com>
2024-04-05 15:33:15 +0200

    feat: implement hook in NinjaComponent

--- src/components/NinjaComponent.jsx
+++ src/components/NinjaComponent.jsx
@@ -1,22 +1,22 @@
-import React, { useState, useEffect } from "react";
+import React, { useRef } from "react";
+import useFetchOnVisible from "../hooks/useFetchOnVisible";
 
 const NinjaComponent = ({ id }) => {
-  const [data, setData] = useState(null);
+  const ref = useRef(null);
 
-  useEffect(() => {
+  const getSource = () =>
     fetch(`https://jsonplaceholder.typicode.com/photos/${id}`) // replace with your API endpoint
-      .then((response) => response.json())
-      .then((data) => setData(data))
-      .catch((error) => console.error("Error:", error));
-  }, []);
+      .then((response) => response.json());
+  const [data, loading] = useFetchOnVisible(ref, getSource);
 
   return (
-    <div style={{border: '1px dashed yellow'}}>
-      {data ? (<>
-        <p>{data.id}</p>
-        <p>{data.title}</p>
-        <img src={data.thumbnailUrl} />
-      </>
+    <div ref={ref} style={{ border: "1px dashed yellow" }}>
+      {data && !loading ? (
+        <>
+          <p>{data.id}</p>
+          <p>{data.title}</p>
+          <img src={data.thumbnailUrl} />
+        </>
       ) : (
         <p>Loading...</p>
       )}
